5564805566a69af6226e8a87aa1b785f86d8436d
Dev Nasser <93291368+Dev-Nasser@users.noreply.github.com>
2022-02-08 14:14:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const nome = document.getElementById("nome").value
+const idade = document.getElementById("Idade").value
+const  linguagem = document.getElementById("linguagem").value
+
+function mostrar (){
+
+    alert(`Olá vejo que o seu nome é ${nome}, sua idade é de ${idade} anos e ja esta aprendendo ${linguagem}!`)
+
+}
+
+
+
+
